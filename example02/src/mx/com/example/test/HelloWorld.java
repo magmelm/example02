e4108d6ad6f5da1042419c3fb0b4b6be592e83fc
missile <missile@missileplex>
2014-10-10 21:38:11 -0500

    Version20141010_2137

---
+++ example02/src/mx/com/example/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package mx.com.example.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+
+	}
+
+}
